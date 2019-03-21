402400a13e98b6a9959d4dea7253296d5f52efcd
judy0131 <xjzhu@cnic.cn>
2019-03-21 10:33:56 +0800

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -6,7 +6,7 @@ function psterm()
         local pid 
         pid=$(ps ax | grep "$1" | grep -v grep | awk '{ print $1 }')
         echo -e "terminating '$1' / process(es):\n$pid"
-        kill -SIGTERM $pid
+        kill -9 $pid
 }
 #ps grep
 ps aux | grep "piflow.api.Main"| grep -v 'grep'
